7f67d08e24c7f2e99722531e04ea457ff25a35fe
Piers Karsenbarg <piers@pulumi.com>
2024-05-01 11:04:01 +0100

    Deleting object

--- index.ts
+++ index.ts
@@ -5,10 +5,10 @@ import * as awsx from "@pulumi/awsx";
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket("my-bucket");
 
-const bucketobject = new aws.s3.BucketObject("bucketobject", {
-    source: new pulumi.asset.FileAsset("file.txt"),
-    bucket: bucket.bucket
-})
+// const bucketobject = new aws.s3.BucketObject("bucketobject", {
+//     source: new pulumi.asset.FileAsset("file.txt"),
+//     bucket: bucket.bucket
+// })
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
